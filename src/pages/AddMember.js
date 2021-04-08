95ad0a57a747c5b55d29d5b316444ec24210a9b7
Ludwin Granados <ludwingranados@gmail.com>
2021-04-08 15:05:48 -0700

    made chages that where requested/ deleted MembersCard, modified AddMember and Member pages

--- src/pages/AddMember.js
+++ src/pages/AddMember.js
@@ -1,6 +1,6 @@
 import { membersData } from '../data/members';
 import { useState, useEffect } from 'react';
-import MembersCard from '../components/MembersCard';
+import { Link } from 'react-router-dom';
 
 const MemberForm = () => {
 
@@ -224,17 +224,42 @@ const MemberForm = () => {
 
 
       
-            <div className="row">
-                <div className="col d-flex flex-row flex-wrap">
-                    {members.map((m, i) =>{
-                        return(
-                            <div key ={i}>
-                                < MembersCard member={m} />
-                            </div>
-                        )
-                    })}
+       <div className="row">
+        <div className="col d-flex flex-row flex-wrap">
+          {members.map((member, i) => {
+            return (
+              <div key={i}>
+                <div className="d-flex m-3">
+                  <div
+                    className="card"
+                    style={{ width: "15rem", border: "white" }}
+                  >
+                    <img
+                      className="card-img-top align-self-center"
+                      src={member.profile_img}
+                      alt="Alkali member"
+                      style={{ borderRadius: "50%", width: "10rem" }}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-text">
+                        {" "}
+                        {member.firstName} {member.lastName}
+                      </h5>
+                      <p className="card-text mb-1">
+                        {" "}
+                        <strong>Role:</strong> {member.role}
+                      </p>
+                      <p className="card-link mb-1">
+                        <Link to={`/members/${member.id}`}>View Details</Link>
+                      </p>
+                    </div>
+                  </div>
                 </div>
-            </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
 
       </div>
     )
